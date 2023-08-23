0737553a2cb030ffcf6f138d4bea5d67bbcb250c
Hildebrando Mendes <brandorocha00@gmail.com>
2023-08-23 19:08:18 -0400

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,8 +1,22 @@
 package main
 
-import "github.com/Bran00/oportuniesgo/router"
+import (
+	"github.com/Bran00/oportuniesgo/config"
+	"github.com/Bran00/oportuniesgo/router"
+)
+
+var (
+	logger config.Logger
+)
 
 func main() {
-  //Initialize Router
+	logger := config.GetLogger("main")
+	//Initialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+	//Initialize Router
 	router.Initialize()
 }
